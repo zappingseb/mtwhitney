239b716b049edca57eb05e1564fb5f21f470d899
zappingseb <sebastian@mail-wolf.de>
2019-07-21 22:21:36 +0200

    first version with python emailer

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# alpine-python-ecmwfapi
+FROM rocker/tidyverse/
+
+MAINTAINER zappingseb "sebastian@mail-wolf.de"
+
+WORKDIR /usr/src/app
+
+RUN R -e "install.packages('RSelenium', repos='https://cran.rstudio.com/') "
+
+COPY /RSelenium.R /tmp/RSelenium.R
+COPY /run_test.R /tmp/run_test.R
+
+RUN pip install pytest-shutil
+RUN pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+
--- run_tests.R
+++ run_tests.R
@@ -1,10 +1,83 @@
 setwd("C:/_wolfs25/git/mtwhitney")
 library(tcltk)
-library(RTest)
+
 Sys.setenv("JAVA_HOME" = "C:/Program Files/Java/jdk1.8.0_201")
-devtools::load_all("C:/_wolfs25/git/RSeleniumTest")
+
+library(RSelenium)
+library(gmailr)
+
+source("RSelenium.R")
+
+sink("my_log.txt")
+
+init_RSelenium("https://www.recreation.gov/permits/233260/",
+    driver_location = "C:/Programme_2/RSelenium/selenium-server-standalone-3.141.59.jar",
+    ie_driver_folder = "C:/Programme_2/RSelenium",
+    browserName = "chrome"
+    )
+
+Sys.sleep(8)
+
+click_element(
+    object = remDr,
+    type_of_search = "id",
+    value = "division-selection-select",
+    iframe = FALSE
+    )
+
+click_element(object = remDr,
+    type_of_search = "css selector",
+    value = ".rec-select-option-button",
+    iframe = FALSE,
+    find = TRUE,
+    which = 2
+    )
+Sys.sleep(2)
+enter_text(object = remDr,
+    type_of_search = "id",
+    value = "number-input-",
+    iframe = FALSE,
+    text_to_type = "1",
+    visibility = FALSE
+    )
+
+month <- get_element_text(
+    object = remDr,
+    type_of_search = "id",
+    value = "CalendarMonth__caption",
+    iframe = FALSE
+    )
+while(!grepl("October",month)) {
+  click_element(object = remDr,
+      type_of_search = "css selector",
+      value = ".DayPickerNavigation__next",
+      iframe = FALSE,
+      find = TRUE,
+      which = 2
+  )
+  month <- get_element_text(
+      object = remDr,
+      type_of_search = "id",
+      value = "CalendarMonth__caption",
+      iframe = FALSE
+  )
+}
 
 
-source("overwrite_expect.R")
+earliest_day <- get_element_text(
+    object = remDr,
+    type_of_search = "css selector",
+    value = ".rec-available-day",
+    iframe = FALSE,
+    which = 1
+)[1]
 
-execute("Sebastian Wolf", project.name = "Crawl Mt Whitney")
+sink()
+
+log <- readLines("my_log.txt")
+
+fileConn<-file("output.txt")
+writeLines(paste0("The earliest day for Mnt Whitney in October is: ",
+        earliest_day, "th of October 2019.\n\n-------------------\nLOG:\n", paste(log, collapse = "\n")), con = fileConn)
+close(fileConn)
+
