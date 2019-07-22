d70dc0e893c50c0be75c4b989988db8ff4a7035e
Sebastian Wolf <sebastian.wolf.sw1@roche.com>
2019-07-22 09:33:18 +0200

    change mtdocker

--- run_tests.R
+++ run_tests.R
@@ -1,7 +1,7 @@
-#setwd("C:/_wolfs25/git/mtwhitney")
+setwd("C:/_wolfs25/git/mtwhitney")
 #library(tcltk)
 
-#Sys.setenv("JAVA_HOME" = "C:/Program Files/Java/jdk1.8.0_201")
+Sys.setenv("JAVA_HOME" = "C:/Program Files/Java/jdk1.8.0_201")
 
 library(RSelenium)
 
@@ -9,13 +9,7 @@ source("RSelenium.R")
 
 sink("my_log.txt")
 
-#init_RSelenium("https://www.recreation.gov/permits/233260/",
-#    driver_location = "C:/Programme_2/RSelenium/selenium-server-standalone-3.141.59.jar",
-#    ie_driver_folder = "C:/Programme_2/RSelenium",
-#    browserName = "chrome"
-#    )
 print("Open Driver")
-system("phantomjs --webdriver=4444")
 remDr <- remoteDriver(browserName = "phantomjs")
 
 print("remDr$open")
@@ -25,67 +19,47 @@ print("navigate")
 remDr$navigate("https://www.recreation.gov/permits/233260/")
 
 Sys.sleep(8)
+tryCatch({
+  el_1 <- remDr$findElements("id", "division-selection-select")
+  el_1$clickElement()
+  el_2 <- remDr$findElements("css selector", ".rec-select-option-button")
+  el_2[[2]]$clickElement()
+  Sys.sleep(1)
 
-click_element(
-    object = remDr,
-    type_of_search = "id",
-    value = "division-selection-select",
-    iframe = FALSE
-    )
+  message("Enter Text")
+  el_3 <- remDr$findElements("id", "number-input-")
+  el_3[[1]]$sendKeysToElement("1")
 
-click_element(object = remDr,
-    type_of_search = "css selector",
-    value = ".rec-select-option-button",
-    iframe = FALSE,
-    find = TRUE,
-    which = 2
-    )
-Sys.sleep(2)
-enter_text(object = remDr,
-    type_of_search = "id",
-    value = "number-input-",
-    iframe = FALSE,
-    text_to_type = "1",
-    visibility = FALSE
-    )
+  message("get Month")
+  month_elem <- remDr$findElements("id", "CalendarMonth__caption")
 
-month <- get_element_text(
-    object = remDr,
-    type_of_search = "id",
-    value = "CalendarMonth__caption",
-    iframe = FALSE
-    )
-while(!grepl("October",month)) {
-  click_element(object = remDr,
-      type_of_search = "css selector",
-      value = ".DayPickerNavigation__next",
-      iframe = FALSE,
-      find = TRUE,
-      which = 2
-  )
-  month <- get_element_text(
-      object = remDr,
-      type_of_search = "id",
-      value = "CalendarMonth__caption",
-      iframe = FALSE
-  )
-}
+  month <- month_elem[[1]]$getElementText()
+
+  message("Month while start")
+  while(!grepl("October", month)) {
+    el_4 <- remDr$findElements("css selector", ".DayPickerNavigation__next")
+    el_4[[2]]$clickElement()
+    month_elem <- remDr$findElements("id", "CalendarMonth__caption")
+    month <- month_elem[[1]]$getElementText()
+  }
+
+  message("Read Early day")
+  day_elem <- remDr$findElements("css selector", ".rec-available-day")[[1]]
+  earliest_day <- day_elem$getElementText()
+
+  sink()
+
+  log <- readLines("my_log.txt")
+
+  fileConn<-file("output.txt")
+  writeLines(paste0("The earliest day for Mnt Whitney in October is: ",
+          earliest_day, "th of October 2019.\n\n-------------------\nLOG:\n", paste(log, collapse = "\n")), con = fileConn)
+  close(fileConn)
 
 
-earliest_day <- get_element_text(
-    object = remDr,
-    type_of_search = "css selector",
-    value = ".rec-available-day",
-    iframe = FALSE,
-    which = 1
-)[1]
+}, error = function(e){
+  remDr$close()
+})
 
-sink()
 
-log <- readLines("my_log.txt")
-
-fileConn<-file("output.txt")
-writeLines(paste0("The earliest day for Mnt Whitney in October is: ",
-        earliest_day, "th of October 2019.\n\n-------------------\nLOG:\n", paste(log, collapse = "\n")), con = fileConn)
-close(fileConn)
 
