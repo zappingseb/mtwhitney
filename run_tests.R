01c808b601369252d7a7ee071d81a8a6af8e2c19
Sebastian Wolf <sebastian.wolf.sw1@roche.com>
2019-09-30 11:14:55 +0200

    upaded with new mt whitney site

--- run_tests.R
+++ run_tests.R
@@ -3,10 +3,11 @@
 library(RSelenium)
 
 source("/tmp/RSelenium.R")
-
 file.remove("/tmp/my_log.txt")
-sink("/tmp/my_log.txt")
+file.remove("/tmp/output.txt")
 
+sink("/tmp/my_log.txt")
+print(date())
 print("Open Driver")
 remDr <- remoteDriver(remoteServerAddr = "seleniumcontainer", browserName = "chrome")
 
@@ -18,61 +19,61 @@ remDr$navigate("https://www.recreation.gov/permits/233260/")
 
 Sys.sleep(8)
 tryCatch({
-  el_1 <- remDr$findElements("id", "division-selection")
-  el_1[[1]]$clickElement()
-  el_2 <- remDr$findElements("css selector", "option[value=406]")
-  el_2[[2]]$clickElement()
-  Sys.sleep(1)
+      el_1 <- remDr$findElements("id", "division-selection")
+      el_1[[1]]$clickElement()
+      el_2 <- remDr$findElements("css selector", "option")
+      el_2[[3]]$clickElement()
+      Sys.sleep(1)
 
-  message("Enter Text")
-  el_3 <- remDr$findElements("id", "number-input-")
-  remDr$executeScript(
-      paste0("arguments[0].setAttribute('value','",
-          "1","');"),
-      list(el_3[[1]]))
-  el_3[[1]]$clearElement()
-  el_3[[1]]$sendKeysToElement(list("1"))
+      message("Enter Text")
+      el_3 <- remDr$findElements("id", "number-input-")
+      remDr$executeScript(
+          paste0("arguments[0].setAttribute('value','",
+              "1","');"),
+          list(el_3[[1]]))
+      el_3[[1]]$clearElement()
+      el_3[[1]]$sendKeysToElement(list("1"))
 
-  message("get Month")
-  month_elem <- remDr$findElements("css selector", "#CalendarMonth__caption strong")
+      message("get Month")
+      month_elem <- remDr$findElements("css selector", ".CalendarMonth_caption strong")
 
-  month <- month_elem[[2]]$getElementText()
+      month <- month_elem[[1]]$getElementText()
 
-  message("Month while start")
-  while(!grepl("October", month)) {
-    el_4 <- remDr$findElements("css selector", ".DayPickerNavigation__next")
-    el_4[[1]]$clickElement()
+      message("Month while start")
+      while(!grepl("October", month)) {
+        el_4 <- remDr$findElements("css selector", ".sarsa-day-picker-range-controller-month-navigation-button.right")
+        el_4[[1]]$clickElement()
 
-    Sys.sleep(1)
-    month_elem <- remDr$findElements("id", "CalendarMonth__caption")
-    month <- month_elem[[2]]$getElementText()
-    print(month)
-  }
+        Sys.sleep(1)
+        month_elem <- remDr$findElements("css selector", ".CalendarMonth_caption")
+        month <- month_elem[[2]]$getElementText()
+        print(month)
+      }
 
-  message("Read Early day")
-  day_elem <- remDr$findElements("css selector", ".rec-available-day")
-  if (length(day_elem) < 1) {
-    earliest_day <- "NODAY"
-  } else {
-    earliest_day <- strsplit(day_elem[[1]]$getElementText()[[1]], split = "\n")[[1]][1]
-  }
+      message("Read Early day")
+      day_elem <- remDr$findElements("css selector", ".rec-available-day")
 
-  sink()
+      if (length(day_elem) < 1) {
+        earliest_day <- "NODAY"
+      } else {
+        earliest_day <- strsplit(day_elem[[1]]$getElementText()[[1]], split = "\n")[[1]][1]
+      }
+      sink()
 
-  remDr$close()
+      remDr$close()
 
-  log <- readLines("/tmp/my_log.txt")
+      log <- readLines("/tmp/my_log.txt")
 
-  fileConn<-file("/tmp/output.txt")
-  writeLines(paste0("The earliest day for Mnt Whitney in ", month[[1]], " is: ",
-          earliest_day, "th of October 2019.\n\n-------------------\nLOG:\n", paste(log, collapse = "\n")), con = fileConn)
-  close(fileConn)
+      fileConn<-file("/tmp/output.txt")
+      writeLines(paste0("The earliest day for Mnt Whitney in ", month[[1]], " is: ",
+              earliest_day, "th of October 2019.\n\n-------------------\nLOG:\n", paste(log, collapse = "\n")), con = fileConn)
+      close(fileConn)
 
 
-}, error = function(e){
-  print(e)
-  sink()
-  remDr$close()
-})
+    }, error = function(e){
+      print(e)
+      sink()
+      remDr$close()
+    })
 
 system("python /tmp/sendmail.py")
